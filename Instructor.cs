2920810c5249bacfde8b6ef1f69d7528663368ff
Joey Driscoll <driscoll.recording@gmail.com>
2019-07-17 13:21:26 -0500

    created an inheritance class called NSSPerson. Instructor and Student classes inherit from NSSPerson.

--- Instructor.cs
+++ Instructor.cs
@@ -2,13 +2,8 @@ using System;
 
 namespace StudentExercises
 {
-    public class Instructor
+    public class Instructor : NSSPerson
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string SlackHandle { get; set; }
-        public int Cohort { get; set; }
-
         public void AssignStudents(Student student, Exercise exercise)
         {
             student.Exercises.Add(exercise);
